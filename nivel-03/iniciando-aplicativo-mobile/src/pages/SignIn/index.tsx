a60e1af3e147a852bbf7e1f9a5eebeb2f8508a98
Vitor de Castro <dev.castrors@gmail.com>
2020-12-10 06:09:17 -0300

    feat: input and button

--- nivel-03/iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
+++ nivel-03/iniciando-aplicativo-mobile/src/pages/SignIn/index.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => {
-  return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu Logon</Title>
-    </Container>
-  );
-};
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+    <Title>Faça seu Logon</Title>
+
+    <Input />
+    <Input />
+  </Container>
+);
 
 export default SignIn;

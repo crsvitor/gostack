0c59ba3b12a52c1485ece1b2cfb65ef3400a0246
Vitor de Castro <dev.castrors@gmail.com>
2020-12-22 23:57:03 -0300

    feat: architecture and DDD

--- nivel-04/arquitetura-e-testes-no-nodejs/src/modules/users/repositories/IUsersRepository.ts
+++ nivel-04/arquitetura-e-testes-no-nodejs/src/modules/users/repositories/IUsersRepository.ts
@@ -5,5 +5,5 @@ export default interface IUsersRepository {
   findById(id: string): Promise<User | undefined>;
   findByEmail(id: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
-  create(data: User): Promise<User>;
+  save(data: User): Promise<User>;
 }

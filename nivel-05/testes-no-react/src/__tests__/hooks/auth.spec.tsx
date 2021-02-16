787817472f6b9e40a9b9e60112f2cfb08126cbe3
Vitor de Castro <dev.castrors@gmail.com>
2021-02-15 21:44:08 -0300

    feat: iniciando testes dos hooks

---
+++ nivel-05/testes-no-react/src/__tests__/hooks/auth.spec.tsx
@@ -0,0 +1,18 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { AuthProvider, useAuth } from '../../hooks/auth';
+
+describe('Auth hook', () => {
+  it('should be able to sign in', () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@example.com.br',
+      password: '123456',
+    });
+
+    console.log(result.current.user);
+    expect(result.current.user.email).toEqual('johndoe@example.com.br');
+  });
+});
